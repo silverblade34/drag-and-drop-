b46366989caac17874891986ebbeed2baf4fae84
silverblade34 <mpacheco.tacay@gmail.com>
2023-04-03 22:55:19 -0500

    dd

--- index.js
+++ index.js
@@ -2,30 +2,89 @@ const containers = document.querySelectorAll('.group-tarj');
 const cards = document.querySelectorAll('.card');
 
 cards.forEach(card => {
-  card.addEventListener('dragstart', dragstart);
-  card.addEventListener('dragend', dragend);
+    card.addEventListener('dragstart', dragstart);
+    card.addEventListener('dragend', dragend);
 });
 
 function dragstart() {
-  this.classList.add('dragging');
+    this.classList.add('dragging');
 }
 
 function dragend() {
-  this.classList.remove('dragging');
+    this.classList.remove('dragging');
 }
 
 containers.forEach(container => {
-  container.addEventListener('dragover', dragover);
-  container.addEventListener('drop', drop);
+    container.addEventListener('dragover', dragover);
+    container.addEventListener('drop', drop);
 });
 
 function dragover(e) {
-  e.preventDefault();
-  this.classList.add('hovered');
+    e.preventDefault();
+    this.classList.add('hovered');
 }
 
 function drop() {
-  const card = document.querySelector('.dragging');
-  this.appendChild(card);
-  this.classList.remove('hovered');
+    const card = document.querySelector('.dragging');
+    this.appendChild(card);
+    this.classList.remove('hovered');
+}
+
+
+
+function prueba(){
+    console.log("---------------1")
+    var tour = {
+        id: "mi-guia",
+        steps: [
+            {
+                title: "Paso 1",
+                content: "Este es el primer paso de la guía",
+                target: "paso1",
+                placement: "right",
+                xOffset: 20,
+                arrowOffset: 60,
+                zindex: 999
+            },
+            {
+                title: "Paso 2",
+                content: "Este es el segundo paso de la guía",
+                target: "paso2",
+                placement: "bottom",
+                xOffset: 20,
+                arrowOffset: 60,
+                zindex: 999
+            }
+        ]
+    };
+    console.log("---------------2")
+    hopscotch.startTour(tour);
+    console.log("---------------3")
+}
+function startGuide() {
+    var tour = {
+        id: "mi-guia",
+        steps: [
+            {
+                title: "Paso 1",
+                content: "Este es el primer paso de la guía",
+                target: "paso1",
+                placement: "right",
+                xOffset: 20,
+                arrowOffset: 60,
+                zindex: 999
+            },
+            {
+                title: "Paso 2",
+                content: "Este es el segundo paso de la guía",
+                target: "paso2",
+                placement: "bottom",
+                xOffset: 20,
+                arrowOffset: 60,
+                zindex: 999
+            }
+        ]
+    };
+
+    hopscotch.startTour(tour);
 }
